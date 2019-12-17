6da6fe2764d6d9f92777d72b802a175dcae62dd2
dabrabgellak <barbaracalle10i3@gmail.com>
2019-12-17 16:21:58 +0000

    Added a comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
