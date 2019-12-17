bc081595424e7ae6807f983c214441e6bf5acf53
dabrabgellak <barbaracalle10i3@gmail.com>
2019-12-17 20:11:48 +0000

    db storage class

---
+++ models/engine/db_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class DBStorage:
+    """ New Engine """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ Instantiation of DBStorage class """
+        self.__engine
