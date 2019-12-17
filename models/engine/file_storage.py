ba8a7438b1b6373ca5ded18f0d4fdd3efc341617
joshuaciencia <joshuaciencia2016@gmail.com>
2019-12-17 17:01:53 +0000

    Delete object and filter by class name

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,16 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        cls_dic = {}
+        for key, value in self.__objects.items():
+            if type(value) is cls:
+                cls_dic[key] = value
+        return cls_dic
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +59,7 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj in self.__objects:
+            self.__objects.remove(obj)
