640143a63198ab60748ee98561d4c2ed3f49dcb9
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-06 03:37:56 +0000

    task 2 deploy to web server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from os.path import exists
+from fabric.operations import put, run
+from fabric.api import env
+"""
+deploy to web server
+"""
+
+env.hosts = [
+            '34.74.29.47',
+            '35.237.30.103'
+        ]
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """ deploy to web server """
+    if not exists(archive_path):
+        return False
+    new_dir = '/data/web_static/releases/{}'.format(archive_path[0:-4])
+    f = archive_path.split('/')[-1]
+    try:
+        put((archive_path), '/tmp/')
+        run('mkdir -p {}'.
+            format(new_dir))
+        run('tar -xzf /tmp/{} -C {}'.
+            format(f, new_dir))
+        run('rm /tmp/{}'.format(f))
+        run('mv {}/web_static/* {}/'.format(new_dir, new_dir))
+        run('rm -rf {}/web_static'.format(new_dir))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(new_dir))
+    except Exception:
+        return False
