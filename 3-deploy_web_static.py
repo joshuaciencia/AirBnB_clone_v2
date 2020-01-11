524b57026077c64614c82258a67148c70d539d7f
root <joshuaciencia@gmail.com>
2020-01-11 03:31:24 +0000

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -18,10 +18,10 @@ def do_pack():
     str_date = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        return local("tar -cvzf versions/web_static_{}.tgz web_static"
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
                      .format(str_date))
-    except Exception as e:
-        print(e)
+	return "versions/web_static_{}.tgz".format(str_date)
+    except Exception:
         return None
 
 
